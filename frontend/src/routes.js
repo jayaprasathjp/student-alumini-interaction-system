cd6f4e71f586b4db3136000762d3e224b5069d4d
jayaprasathjp <jayaprasathjp44@gmail.com>
2024-05-14 23:24:25 +0530

    route setting

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,7 @@
 import Dashboard from "layouts/dashboard";
 import Students from "layouts/students";
 import StudentInteraction from "layouts/student_interaction";
-import Alumnis from "layouts/aluminis";
+import Alumnis from "layouts/alumnis";
 import Program from "layouts/program";
 import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
@@ -10,7 +10,7 @@ import SignUp from "layouts/authentication/sign-up";
 // @mui icons
 import Icon from "@mui/material/Icon";
 const id="alumni";
-const routes = [
+const routes =(id=="staff")? [
   {
     type: "collapse",
     name: "Dashboard",
@@ -75,6 +75,53 @@ const routes = [
     route: "/authentication/sign-up",
     component: <SignUp />,
   },
-];
+]:[{
+  type: "collapse",
+  name: "Dashboard",
+  key: "dashboard",
+  icon: <Icon fontSize="small">dashboard</Icon>,
+  route: "/dashboard",
+  component: <Dashboard />,
+},
+{
+  type: "collapse",
+  name: "Student Interaction",
+  key: "students-interaction",
+  icon: <Icon fontSize="small">add_reaction_icon</Icon>,
+  route: "/students-interaction",
+  component: <StudentInteraction />,
+},
+{
+  type: "collapse",
+  name: "Alumni",
+  key: "alumnis",
+  icon: <Icon fontSize="small">hail</Icon>,
+  route: "/alumnis",
+  component: <Alumnis />,
+},
+{
+  type: "collapse",
+  name: "Program",
+  key: "program",
+  icon: <Icon fontSize="small">receipt_long</Icon>,
+  route: "/program",
+  component: <Program />,
+},
+{
+  type: "collapse",
+  name: "Profile",
+  key: "profile",
+  icon: <Icon fontSize="small">person</Icon>,
+  route: "/profile",
+  component: <Profile />,
+},
+{
+  type: "collapse",
+  name: "Sign Out",
+  key: "sign_out",
+  icon: <Icon fontSize="small">login</Icon>,
+  route: "/authentication/sign-in",
+  component: <SignIn />,
+}];
 
 export default routes;
